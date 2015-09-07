d22d52ecae8d886c0514ab9de4dd70fa991d0f80
Floris Lambrechts <floris.lambrechts@onsemi.com>
2015-09-07 12:54:04 +0200

    Security: pretect admin pages against CSRF attacks using Flask-WTF

--- data/admin.py
+++ data/admin.py
@@ -5,11 +5,16 @@ from data.datamodel import Player, Tag, Challenge, Game, Shout
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_wtf import Form as SafeForm
+
+class SafeModelView(ModelView):
+    from_base_class = SafeForm
 
 # admin interface
 admin = Admin(app)
-admin.add_view(ModelView(Player, db.session))
-admin.add_view(ModelView(Tag, db.session))
-admin.add_view(ModelView(Challenge, db.session))
-admin.add_view(ModelView(Game, db.session))
-admin.add_view(ModelView(Shout, db.session))
+admin.add_view(SafeModelView(Player, db.session))
+admin.add_view(SafeModelView(Tag, db.session))
+admin.add_view(SafeModelView(Challenge, db.session))
+admin.add_view(SafeModelView(Game, db.session))
+admin.add_view(SafeModelView(Shout, db.session))
+
