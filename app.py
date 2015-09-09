9c666ae6273270526e6d69b6b6d209be74a742be
Floris Lambrechts <floris.lambrechts@onsemi.com>
2015-09-09 08:53:45 +0200

    Install Flask-DebugToolbar
    Is only active in debug mode.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+
 import configuration
 
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 # application with configuration
 app = Flask(__name__)
 app.config.from_object(configuration)
+toolbar = DebugToolbarExtension(app)
+
